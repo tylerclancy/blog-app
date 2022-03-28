631a67a3c6709fc2f002a143ff3501194b6fbf9e
Tyler <tylerclancy030@gmail.com>
2022-03-28 16:40:58 -0500

    Add authentication via OAuth with Google Sign in

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,40 @@
-export default function Enter() {
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
+  // 1. User signed out <SignInButton />
+  // 2. User signed in, but missing username <UsernameForm />
+  // 3. User signed in, has username <SignOutButton />
+
   return (
     <div>
-      <h1>Sign up</h1>
+      {user ?
+        !username ? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
     </div>
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  }
+
+  return (
+    <button className='btn-google' onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
   )
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+
 }
