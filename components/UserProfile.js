522c60058b799f117a1add4f5b18dcd7b22595ec
Tyler <tylerclancy030@gmail.com>
2022-03-29 01:28:23 -0500

    Implement server side rendering to fetch data from server

---
+++ components/UserProfile.js
@@ -0,0 +1,12 @@
+// UI component for user's profile.
+export default function UserProfile() {
+  return (
+    <div className='box-center'>
+      <img src={user.photoURL} className='card-img-center' />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName}</h1>
+    </div>
+  );
+}
--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,37 @@
-export default function UserProfile() {
+import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
+import { getUserWithUsername } from '../../lib/firebase';
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+
+      posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // Will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <div>
-      <h1>User profile</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </div>
-  )
+  );
 }
