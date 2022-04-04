d67411b31a4efae7b8ff313c31d0594a4815b8eb
Tyler <tylerclancy030@gmail.com>
2022-04-03 19:55:46 -0500

    Add form validation using react-hook-forms

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -57,7 +57,9 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }) {
-  const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
+  const { register, handleSubmit, reset, watch, formState, errors } = useForm({ defaultValues, mode: 'onChange' });
+
+  const { isValid, isDirty } = formState;
 
   const updatePost = async ({ content, published }) => {
     await postRef.update({
@@ -82,14 +84,20 @@ function PostForm({ defaultValues, postRef, preview }) {
       }
 
       <div className={preview ? styles.hidden : styles.controls}>
-        <textarea name='content' ref={register}></textarea>
+        <textarea name='content' ref={register({
+          maxLength: {value: 20000, message: 'content is too long'},
+          minLength: {value: 10, message: 'content is too short'},
+          required: {value: true, message: 'content is required'}
+        })}></textarea>
+
+        {errors.content && <p className='text-danger'>{errors.content.message}</p>}
 
         <fieldset>
           <input className={styles.checkbox} name='published' type='checkbox' ref={register} />
           <label>Published</label>
         </fieldset>
 
-        <button type='submit' className='btn-green'>
+        <button type='submit' className='btn-green' disabled={!isDirty || !isValid}>
           Save Changes
         </button>
       </div>
